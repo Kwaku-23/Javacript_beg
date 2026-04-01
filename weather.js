001b614a33a58de868b3965024373f349afd5738
Kwaku <yeboahasantekwaku@gmail.com>
2026-04-01 03:53:43 +0000

    Work 2 done

--- weather.js
+++ weather.js
@@ -1,24 +1,19 @@
-const select = document.querySelector("select");
-const para = document.querySelector("p");
+function showPrimes(n) {
 
-select.addEventListener("change", setWeather);
+  for (let i = 2; i < n; i++) {
+    if (!isPrime(i)) continue;
 
-function setWeather() {
-  const choice = select.value;
-
-  if (choice === "sunny") {
-    para.textContent =
-      "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
-  } else if (choice === "rainy") {
-    para.textContent =
-      "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long.";
-  } else if (choice === "snowing") {
-    para.textContent =
-      "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
-  } else if (choice === "overcast") {
-    para.textContent =
-      "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
-  } else {
-    para.textContent = "";
+    alert(i);  // a prime
   }
 }
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if ( n % i == 0) return false;
+  }
+  return true;
+}
+
+showPrimes(n)
+
+isPrime(n)
