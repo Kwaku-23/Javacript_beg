8f1bc0bae80e438264f90df092dda59bba2af2c0
Kwaku <yeboahasantekwaku@gmail.com>
2026-05-06 10:42:41 +0000

    I am almost

---
+++ DOM.js
@@ -0,0 +1,18 @@
+const redP=document.createElement("p");
+redP.textContent="Hey I'm red";
+redP.style.color="red";
+container.appendChild(redP);
+
+const blueP=document.createElement("h3");
+blueP.textContent="I'm a blue h3!";
+blueP.style.color="blue";
+blueP.appendChild();
+
+
+const divd=document.createElement("div");
+const head=document.createElement("h1");
+head.textContent="I'm in a div";
+ const para=document.createElement("p");
+ para.textContent="ME TOO!";
+divd.appendChild("para");
+divd.appendChild("head");
